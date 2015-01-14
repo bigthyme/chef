23390fe2e961ba07976fda5eb87a2a36e5abda92
bigthyme <asinghavong@gmail.com>
2015-01-14 10:23:55 -0800

    updatating templates

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -1,16 +1,42 @@
-# Constants
-WEB_PATH = "/var/www"
-
-# install httpd
 package "httpd" do
-    action :install
+  action :install
 end
 
 service "httpd" do
-    action [:enable, :start]
+  action [ :enable, :start ]
 end
 
-cookbook_file "#{WEB_PATH}/html/index.html" do
-    source "index.html"
+execute "mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.bak" do
+  only_if do
+    File.exist?("/etc/httpd/conf.d/welcome.conf")
+  end
+  notifies :restart, "service[httpd]"
+end
+
+node['apache']['sites'].each do |site_name, site_data|
+  document_root = "/srv/apache/#{site_name}"
+
+  template "/etc/httpd/conf.d/#{site_name}.conf" do
+    source "custom.erb"
     mode "0644"
+    variables(
+      :document_root => document_root,
+      :port => site_data['port']
+    )
+    notifies :restart, "service[httpd]"
+  end
+
+  directory document_root do
+    mode "0755"
+    recursive true
+  end
+
+  template "#{document_root}/index.html" do
+    source "index.html.erb"
+    mode "0644"
+    variables(
+      :site_name => site_name,
+      :port => site_data['port']
+    )
+  end
 end
